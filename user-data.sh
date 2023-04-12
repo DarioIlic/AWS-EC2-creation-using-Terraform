cdec6a412d23844f22b9879bd6b321966e1c9944
Dilic26 <darioilic13@gmail.com>
2023-04-12 13:16:49 +0200

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -5,4 +5,4 @@ sudo systemctl start apache2
 sudo systemctl enable apache2
 
 rm -rf /var/www/html/index.html
-echo ' "Hello Dhimahi" ' > /var/www/html/index.html
+echo ' "Hello Dhimahi" ' >> /var/www/html/index.html
